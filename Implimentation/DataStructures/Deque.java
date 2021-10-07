36d687ef8fba848c944b2a331e51b909e98a082e
iampharikrishnan <iampharikrishnan@gmail.com>
2021-10-07 14:05:11 +0530

    Deque implimented

---
+++ Implimentation/DataStructures/Deque.java
@@ -0,0 +1,56 @@
+package Implimentation.DataStructures;
+
+public class Deque {
+    Node front = null;
+    Node rear = null;
+
+    public boolean offer(int data){
+        if(front==null){
+            front = new Node(data);
+            rear = front;
+        }
+        else{
+            front = new Node(data,front);
+            front.next.previous = front;
+        }
+        return true;
+    }
+
+    public int pole(){
+        if(rear==null) return Integer.MIN_VALUE;
+        Node node = rear;
+        rear = rear.previous;
+        return node.data; 
+    }
+
+    public int pop(){
+        if(front==null) return Integer.MIN_VALUE;
+        Node node = front;
+        front = front.previous;
+        return node.data; 
+    }
+
+    public int peek(){
+        if(rear==null) return Integer.MIN_VALUE;
+        return rear.data;
+    }
+
+    public boolean empty(){
+        return (rear==null);
+    }
+    
+    class Node{
+        int data;
+        Node next = null;
+        Node previous = null;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
